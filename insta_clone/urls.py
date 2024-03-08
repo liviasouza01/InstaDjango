480ec7c9c153ff26d7c4f4f2f5fc6e7f7ec9e099
Livia Souza <liviaa.sa@hotmail.com>
2024-03-08 15:05:45 -0300

    urls settings

--- insta_clone/urls.py
+++ insta_clone/urls.py
@@ -13,7 +13,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django import urls
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -21,10 +20,10 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('home.urls')),
+    path('', include('authentication.urls')),  # Authentication URLs as root
+    path('home/', include('home.urls')),  # Assuming this is your home page after authentication
     path('post/', include('post.urls')),
     path('social/', include('social.urls')),
-    path('', include('authentication.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
