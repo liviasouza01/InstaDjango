7839545efc473e5a68e67adc1d72411e8fe21b4a
Livia Souza <liviaa.sa@hotmail.com>
2024-03-09 21:33:19 -0300

    static bug

--- insta_clone/settings.py
+++ insta_clone/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#t(h6j+00v_53-$xm=d%y-=bm%vq+_zt@@7@qks_6b+t=+yd0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['instadjango.onrender.com', '127.0.0.1']
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
