dfdf21fd5248b83ec73c97ca3e452ffc7e357c20
Livia Souza <liviaa.sa@hotmail.com>
2024-03-10 13:05:04 -0300

    data query otimized
    Used prefetch and select_related on followers views

--- follow/views.py
+++ follow/views.py
@@ -8,11 +8,8 @@ from django.contrib.auth.models import User
 from follow.models import Follow, Stream
 from post.models import Post
 
-# Create your views here.
-
 @login_required
 def follow(request, username, option):
-    # Get the current logged-in user
     user = request.user
     # Get the user who is being followed/unfollowed
     following = get_object_or_404(User, username=username)
@@ -20,21 +17,21 @@ def follow(request, username, option):
     try:
         # Check if there is already a follow relationship between the users
         f, created = Follow.objects.get_or_create(followers=user, following=following)
-        if int(option) == 0:  # If option is 0, unfollow the user
+
+        # If option is 0, unfollow the user
+        if int(option) == 0:
             # Delete the follow relationship
             f.delete()
-            # Delete all posts from the user being followed in the current user's stream
-            Stream.objects.filter(following=following, user=user).all().delete()
-        else:  # If option is not 0, follow the user
-            # Get all posts from the user being followed
-            posts = Post.objects.all().filter(user=following)
+            Stream.objects.filter(following=following, user=user).delete()
+        else:
+            # Get all posts from the user being followed with related user info
+            posts = Post.objects.prefetch_related('user').filter(user=following)
             with transaction.atomic():
-                # Add each post to the current user's stream
-                for post in posts:
-                    stream = Stream(post=post, user=user, following=following, date=post.created_at)
-                    stream.save()
-        # Redirect to the profile page of the user being followed/unfollowed
+                # Create Stream objects for each post
+                streams = [Stream(post=post, user=user, following=following, date=post.created_at) for post in posts]
+                # Bulk create Stream objects
+                Stream.objects.bulk_create(streams)
+
         return HttpResponseRedirect(reverse('profile:profile', args=[username]))
     except User.DoesNotExist:
-        # Redirect to the profile page of the user being followed/unfollowed if the user doesn't exist
         return HttpResponseRedirect(reverse('profile:profile', args=[username]))
