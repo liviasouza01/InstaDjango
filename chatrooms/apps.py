3c43ebf1ae75c848484acf78f02ed39846da7c01
Livia Souza <liviaa.sa@hotmail.com>
2024-03-11 12:38:09 -0300

    Chatroom bugs fixed
    Design improved and added the option to post clicking ENTER too

--- chatrooms/apps.py
+++ chatrooms/apps.py
@@ -1,6 +1,5 @@
 from django.apps import AppConfig
 
 
-class ChatroomConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
+class ChatConfig(AppConfig):
     name = 'chatrooms'
