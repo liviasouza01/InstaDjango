cd335147363b3bb9b992c1c03c185eaa908779f2
Livia Souza <liviaa.sa@hotmail.com>
2024-03-20 23:52:30 -0300

    gitignore created

--- insta_clone/settings.py
+++ insta_clone/settings.py
@@ -97,28 +97,9 @@ CHANNEL_LAYERS = {
     },
 }
 
-
-# Database
-# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
-'''
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-'''
-
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-
     }
-
-
-
-# Password validation
-# https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
