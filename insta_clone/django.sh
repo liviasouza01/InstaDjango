07c70e173ff0e74677da2a4a57d34d376539f8d2
Livia Souza <liviaa.sa@hotmail.com>
2024-03-23 11:36:05 -0300

    Docker config

---
+++ insta_clone/django.sh
@@ -0,0 +1,10 @@
+echo "Create migrations"
+python manage.py makemigrations djangoapp
+echo "=================================="
+
+echo "Migrate"
+python manage.py migrate
+echo "=================================="
+
+echo "Start server"
+python manage.py runserver 0.0.0.0:8000
